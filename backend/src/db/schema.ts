88d3bcfbdf7f12dcf160c0546062b8731940b164
laurin42 <laurin.schmidt@yahoo.com>
2025-06-12 15:55:24 +0200

    feat: add adatabase schema with drizzle-orm

---
+++ backend/src/db/schema.ts
@@ -0,0 +1,14 @@
+import { pgTable, serial, timestamp, uuid, text } from "drizzle-orm/pg-core";
+
+export const workSessions = pgTable("work_sessions", {
+    id: serial("id").primaryKey(),
+    userId: uuid("user_id").notNull(),
+    startTime: timestamp("start_time").notNull(),
+    endTime: timestamp("end_time"),
+    note: text("note"),
+
+
+
+
+
+});
